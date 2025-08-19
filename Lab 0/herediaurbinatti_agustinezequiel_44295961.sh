b6b256747e7c175d28d66a414bb5f1017b80d8ea
Agustin Heredia <agustin.heredia.urbinatti@mi.unc.edu.ar>
2025-08-18 23:24:28 -0300

    cd de comandos arreglados y generalizados

--- Lab 0/herediaurbinatti_agustinezequiel_44295961.sh
+++ Lab 0/herediaurbinatti_agustinezequiel_44295961.sh
@@ -10,16 +10,16 @@ cat /proc/cpuinfo | grep "cpu cores" | wc -l
 curl -s https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv | cut -d ';' -f2 | tr 'A-Z' 'a-z' | tr ' ' '_' | grep -v "Identity" | grep -v "^$" | sed '1d' > superheroes_usuarios.txt
 
 # EJERCICIO 4A: Día de máxima temperatura en Córdoba
-cat datos/weather_cordoba.in | sort -k5 -n -r | head -1 | awk '{print $1"-"$2"-"$3}'
+cat weather_cordoba.in | sort -k5 -n -r | head -1 | awk '{print $1"-"$2"-"$3}'
 
 # EJERCICIO 4B: Día de mínima temperatura en Córdoba
-cat datos/weather_cordoba.in | sort -k6 -n | head -1 | awk '{print $1"-"$2"-"$3}'
+cat weather_cordoba.in | sort -k6 -n | head -1 | awk '{print $1"-"$2"-"$3}'
 
 # EJERCICIO 5: Ordenar jugadores de tenis por ranking
-cat datos/atpplayers.in | sort -k3 -n 
+cat atpplayers.in | sort -k3 -n 
 
 # EJERCICIO 6: Ordenar tabla de Superliga por puntos y diferencia de goles
-awk '{print $0, ($7-$8)}' datos/superliga.in | sort -k2,2nr -k9,9nr | cut -d' ' -f1-8
+awk '{print $0, ($7-$8)}' superliga.in | sort -k2,2nr -k9,9nr | cut -d' ' -f1-8
 
 # EJERCICIO 7: MAC address de la placa WiFi del equipo
 ip link show | grep -o -E '([0-9a-f]{2}:){5}[0-9a-f]{2}'
