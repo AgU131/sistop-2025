afa2c2ec6ba75f40c232c7961ed5845fb1ed186d
Agustin <82238717+AgU131@users.noreply.github.com>
2025-08-18 23:17:39 -0300

    Add files via upload

--- Lab 0/extra/notes.md
+++ Lab 0/extra/notes.md
@@ -23,3 +23,14 @@ Ejercicio 4
 Ejercicio 5
 
 
+Ejercicio 6
+
+
+Ejercicio 7
+
+
+Ejercicio 8
+
+
+Ejercicio 8
+    ffmpeg: 
--- Lab 0/herediaurbinatti_agustinezequiel_44295961.sh
+++ Lab 0/herediaurbinatti_agustinezequiel_44295961.sh
@@ -7,29 +7,28 @@ cat /proc/cpuinfo | grep "model name" | head -1
 cat /proc/cpuinfo | grep "cpu cores" | wc -l
 
 # EJERCICIO 3: Lista de nombres de usuarios para red social de superhéroes
-curl -s https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv | grep -v "Identity" | grep -v | cut -d ',' -f2 | tr '[:upper:]' '[:lower:]' | sed 's/ /_/g' > superheroes_usuarios.txt
-curl -s https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv | cut -d',' -f2 | grep -v "Identity" | grep -v '^$' | tr '[:upper:]' '[:lower:]' | tr ' ' '_' > superheroes_usuarios.txt
+curl -s https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv | cut -d ';' -f2 | tr 'A-Z' 'a-z' | tr ' ' '_' | grep -v "Identity" | grep -v "^$" | sed '1d' > superheroes_usuarios.txt
 
 # EJERCICIO 4A: Día de máxima temperatura en Córdoba
-[su comando aquí]
+cat datos/weather_cordoba.in | sort -k5 -n -r | head -1 | awk '{print $1"-"$2"-"$3}'
 
 # EJERCICIO 4B: Día de mínima temperatura en Córdoba
-[su comando aquí]
+cat datos/weather_cordoba.in | sort -k6 -n | head -1 | awk '{print $1"-"$2"-"$3}'
 
 # EJERCICIO 5: Ordenar jugadores de tenis por ranking
-[su comando aquí]
+cat datos/atpplayers.in | sort -k3 -n 
 
 # EJERCICIO 6: Ordenar tabla de Superliga por puntos y diferencia de goles
-[su comando aquí]
+awk '{print $0, ($7-$8)}' datos/superliga.in | sort -k2,2nr -k9,9nr | cut -d' ' -f1-8
 
 # EJERCICIO 7: MAC address de la placa WiFi del equipo
-[su comando aquí]
+ip link show | grep -o -E '([0-9a-f]{2}:){5}[0-9a-f]{2}'
 
 # EJERCICIO 8: Renombrar archivos de subtítulos
-[su comando aquí]
+for f in serie_prueba/*_es.srt; do mv "$f" "${f%_es.srt}.srt"; done
 
 # EJERCICIO 9A: Recortar video con ffmpeg
-[su comando aquí]
+ffmpeg -ss 00:00:10 -i prueba1.mp4 -to 00:00:30 -c copy recorte_prueba1.mp4
 
 # EJERCICIO 9B: Mezclar audio con ffmpeg
-[su comando aquí]
+ffmpeg -i audio1.mp3 -i audio2.mp3 -filter_complex amix=inputs=2 output_audios.mp3
