bf934bfa0cdf7146611a098bd38453f83b9232f7
Agustin <82238717+AgU131@users.noreply.github.com>
2025-08-18 21:04:19 -0300

    Add files via upload

--- Lab 0/herediaurbinatti_agustinezequiel_44295961.sh
+++ Lab 0/herediaurbinatti_agustinezequiel_44295961.sh
@@ -7,7 +7,7 @@ cat /proc/cpuinfo | grep "model name" | head -1
 cat /proc/cpuinfo | grep "cpu cores" | wc -l
 
 # EJERCICIO 3: Lista de nombres de usuarios para red social de superhéroes
-[su comando aquí]
+curl -s https://raw.githubusercontent.com/dariomalchiodi/superhero-datascience/master/content/data/heroes.csv | cut -d',' -f2 | grep -v "Identity" | grep -v '^$' | tr '[:upper:]' '[:lower:]' | tr ' ' '_' > superheroes_usuarios.txt
 
 # EJERCICIO 4A: Día de máxima temperatura en Córdoba
 [su comando aquí]
